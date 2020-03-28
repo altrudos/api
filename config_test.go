2e19e9b5cb46547689573afafa082870c4c1be43
Bryan Cua <bryan@luxe.technology>
2020-03-28 12:02:34 -0700

    Config file & test

---
+++ config_test.go
@@ -0,0 +1,19 @@
+package charityhonor
+
+import "testing"
+
+func TestConfig(t *testing.T) {
+
+	c, err := ParseConfig("./config.toml")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if c.JustGiving.Mode != "staging" {
+		t.Fatal("Expecting mode to be staging")
+	}
+	if c.JustGiving.AppId != "some-app-id" {
+		t.Fatal("Expecting app id to be some-app-id")
+	}
+
+}
