104c977df1ec26f49ce96b7e9c41ca9901c0515b
Colin Kierans <kierans@monstercat.com>
2020-04-11 09:01:29 -0700

    Add num donations to drive

--- sql/create-views.sql
+++ sql/create-views.sql
@@ -4,6 +4,7 @@ WITH sums AS (
     SELECT drive_id,
            SUM(usd_amount) AS usd_amount_total,
            SUM(donor_amount) AS donor_amount_total,
+           COUNT(*) as num_donations,
            most_recent_usd_amount,
            most_recent_donor_amount,
            most_recent_time
@@ -26,6 +27,7 @@ SELECT drives.*,
        COALESCE(sums.donor_amount_total, 0)       AS donor_amount_total,
        COALESCE(sums.most_recent_donor_amount, 0) AS most_recent_donor_amount,
        COALESCE(sums.most_recent_usd_amount, 0) AS most_recent_usd_amount,
+       COALESCE(sums.num_donations, 0) AS num_donations,
        sums.most_recent_time
 FROM drives
          LEFT JOIN sums ON sums.drive_id = drives.id;
